534e78cc6bd9c07fba668f5a330208e029c40bec
Gary Bermudez and Justin Haupt <gary@>
2019-01-15 13:06:35 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  var terms = ['JavaScript','Operators', 'Variables', 'Functions'];
+  var definitions = ['Definition1','Definition2', 'Definition3', 'Definition4'];
+
+  // Initialize first card
+  $('#term').text(terms[0]);
+  $('#def').text(definitions[0]);
+vvbv
+
+
+
+
+
+});
